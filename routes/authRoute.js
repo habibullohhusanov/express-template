feb1bb286b9a9494d977d8ebcdaca568db29ce9d
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-02-08 21:14:58 +0500

    1.0.2 bug fixed

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { login, logout, register, user } from "../controllers/authController.js";
+import { login, /*logout,*/ register, user } from "../controllers/authController.js";
 
 // star
 const router = express.Router();
 
 router.post("/user", user);
 router.post("/login", login);
-router.post("/logout", logout);
+// router.post("/logout", logout);
 router.post("/register", register);
 
 // end
