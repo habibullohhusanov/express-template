f10d08aa66ac8ae03ec603e29bfb8340b7a3fce3
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-04-19 18:22:56 +0500

    1.1.1

--- seeders/index.js
+++ seeders/index.js
@@ -3,11 +3,15 @@ import connect from "../config/dbConfig.js";
 
 const seeder = async () => {
     try {
-        connect();
+        await connect();
 
         await userSeeder();
+
+        console.log("Seeded");
+        process.exit(0);
     } catch (error) {
         console.log(error)
+        process.exit(0);
     }
 }
 
--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -3,16 +3,15 @@ import User from "../models/userModel.js";
 const userSeeder = async () => {
     try {
         await User.deleteMany({});
-        var user = new User(
-            {
-                name: "Admin",
-                email: "admin@gmail.com",
-                password: "2407"
-            });
+        var user = new User({
+            name: "Admin",
+            email: "admin@gmail.com",
+            password: "2407"
+        });
         await user.save();
     } catch (error) {
         console.error('User seeder don\'t work:', error);
     }
 }
-userSeeder();
+
 export default userSeeder;
