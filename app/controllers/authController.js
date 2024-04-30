7cfec6a32d439e7a96cf268f8748fed6a6e9e859
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-04-30 23:49:37 +0500

    2.1.0

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,104 +1,139 @@
+import { loginRequest, registerRequest } from "../requests/authRequest.js";
+import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import User from "../models/userModel.js"
+import UserResource from "../resources/userResource.js";
 
 export const login = async (req, res) => {
     try {
+        const loginData = req.body;
         const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
+        const { error } = loginRequest.validate(loginData);
+        if (error) {
+            return res.status(400).json({
+                tatus: false,
+                data: [],
+                token: null,
+                message: error.message
+            });
+        } else {
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(401).json({
+                    status: false,
+                    data: [],
+                    token: null,
+                    message: "Data incorrect"
+                });
+            }
+            const isCheck = user.checkPassword(password);
+            if (isCheck) {
+                const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
+                    expiresIn: "2d"
+                });
+                const data = new UserResource(user);
+                return res.status(200).json({
+                    status: true,
+                    data: data,
+                    token: token,
+                    message: "Account logged in"
+                });
+            }
             return res.status(401).json({
                 status: false,
                 data: [],
                 token: null,
-                message: `Data incorrect`,
-            });
-        }
-        let isCheck = user.checkPassword(password);
-        if (isCheck) {
-            const token = jwt.sign({userId: user._id}, process.env.JWT_TOKEN, {
-                expiresIn: "1d"
-            });
-            return res.status(200).json({
-                status: true,
-                data: user,
-                token: token,
-                message: "User"
-            });
-        }
-        return res.status(401).json({
-            status: false,
-            data: [],
-            token: null,
-            message: `Data incorrect`,
-        });
-    } catch (error) {
-        return res.status(500).json({
-            status: false,
-            data: [],
-            token: null,
-            message: error.message + " catch",
-        });
-    }
-}
-// export const logout = async (req, res) => {
-//    //
-// }
-export const user = async (req, res) => {
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-        const user = await User.findOne({ _id: decoded.userId });
-        if (!user) {
-            return res.status(401).json({
-                status: false,
-                data: [],
-                message: `Not authorized. Invalid token.`,
-            });
-        } else {
-            return res.status(200).json({
-                status: true,
-                data: user,
-                message: `User finded`,
+                message: "Data incorrect"
             });
         }
     } catch (error) {
         return res.status(500).json({
             status: false,
             data: [],
-            message: error.message + " catch",
+            token: null,
+            message: error.message
         });
     }
 }
 export const register = async (req, res) => {
     try {
+        const registerData = req.body;
         const { name, email, password } = req.body;
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(500).json({
+        const { error } = registerRequest.validate(registerData);
+        if (error) {
+            return res.status(400).json({
                 status: false,
                 data: [],
                 token: null,
-                message: `${email} email allready exsist`,
+                message: error.message
+            });
+        } else {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res.status(500).json({
+                    status: false,
+                    data: [],
+                    token: null,
+                    message: `${email} alredy exsist`
+                });
+            }
+            const newUser = new User({
+                name, email, password
+            });
+            user = await newUser.save();
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
+                expiresIn: "2d"
+            });
+            const data = new UserResource(user);
+            return res.status(201).json({
+                status: true,
+                date: data,
+                token: token,
+                message: "Created"
             });
         }
-        const newUser = new User({
-            name, email, password
-        });
-        user = await newUser.save();
-        const token = jwt.sign({userId: user._id}, process.env.JWT_TOKEN, {
-            expiresIn: "1d"
-        });
-        return res.status(201).json({
-            status: true,
-            data: user,
-            token: token,
-            message: "Yaratildi"
-        });
     } catch (error) {
         return res.status(500).json({
             status: false,
             data: [],
             token: null,
-            message: error.message,
+            message: error.message
+        });
+    }
+}
+export const logout = async (req, res) => {
+
+}
+export const user = async (req, res) => {
+    try {
+        const data = new UserResource(req.user);
+        return res.status(200).json({
+            status: true,
+            data: data,
+            message: "User"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            data: [],
+            message: error.message
+        });
+    }
+}
+export const verify = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id);
+        user.userVerify();
+        const data = new UserResource(user);
+        return res.status(200).json({
+            status: true,
+            data: data,
+            message: "User"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            data: [],
+            message: error.message
         });
     }
 }
